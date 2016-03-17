cb53b821bc842a58696fdbf0857d7c6ec444f730
Jeffrey Tunches <jeffreytunches@gmail.com>
2016-03-17 02:19:49 -0700

    early morn 3-17-16 commit

---
+++ udemy/wdb/w01/d03/html-basics/js/main.js
@@ -0,0 +1,50 @@
+
+
+// profile details
+// global ID
+var modelId = ()
+var modelProfile = ()
+//img
+var ffm-img1 = ()
+var ffm-img2 = ()
+var ffm-img3 = ()
+var ffm-img4 = ()
+//stats
+var ffm-fistName = ()
+var ffm-lastName = ()
+var ffm-dob = ()
+var ffm-age = ()
+var ffm-height = ()
+var ffm-bust-cup = ()
+var ffm-waist = ()
+var ffm-dress = ()
+var ffm-shoe = ()
+//contact
+var ffm-email = ()
+var ffm-phone = ()
+var ffm-address = ()
+var ffm-city = ()
+var ffm-state = ()
+var ffm-zip = ()
+var ffm-country = ()
+var ffm-sch = ()
+//mgmt
+var ffm-ag = ()
+var ffm-agContact = ()
+var ffm-agEmail = ()
+var ffm-agPhone = ()
+var ffm-mgr = ()
+var ffm-mgrContact = ()
+var ffm-mgrEmail = ()
+var ffm-mgrPhone = ()
+//web-social
+var ffm-web = ()
+var ffm-blog = ()
+var ffm-linked = ()
+var ffm-fb = ()
+var ffm-insta = ()
+var ffm-yt = ()
+var ffm-tumblr = ()
+var ffm-twitter = ()
+
+
